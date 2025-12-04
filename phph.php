c0d9ec08c3fbdf67b978de5fe9d73d32a593efe4
askalemariam_tech <askalemariamaman1@gmail.com>
2025-12-04 20:22:20 +0300

    this is commit

---
+++ phph.php
@@ -0,0 +1,56 @@
+T𝑖?, [07/11/2025 1:58 ከሰዓት]
+<html>
+<body>
+<?php
+// $i=10;
+// $i++;
+// echo $i."<br>";   //11
+// $j=($i++)+5;
+// echo $i." ".$j."<br>"; //12    16
+// echo $i++."<br>"; //12
+// $j=++$i+2;
+// echo $i." ".$j."<br>";//14   16
+// echo (4&5);//4
+// $mark=40;
+// if($mark>50)
+  // echo"pass";
+// else
+  // echo"fail";
+// echo"<br>";
+// if($mark>90)
+  // echo"A";
+// else if($mark>80)
+  // echo"B";
+// else if($mark>70)
+  // echo"C";
+// else if($mark>60)
+  // echo"D";
+// else
+  // echo"F";
+
+echo"for-loop<br>";
+for($i=1;$i<=10;$i+=2)
+  echo $i." ";
+echo"<br>";
+for($i=64;$i>=1;$i/=2)
+  echo $i." ";
+echo"<br> do-while <br>";
+$i=1;
+do{
+  echo $i." ";
+  $i++;
+}while($i<=10);
+echo"<br> while loop <br>";
+$i=1;
+while($i<=10){
+  echo $i." ";
+  $i++;
+}
+echo"<br> foreach loop <br>";
+foreach(range(10,1,-2) as $i)
+{
+echo $i." ";
+}
+?>
+
+</body></html>
